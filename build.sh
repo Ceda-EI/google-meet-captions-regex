d2d435527e96a876c1eec54c2db509df52fa5606
Ceda EI <ceda_ei@webionite.com>
2020-09-15 18:32:12 +0530

    Add build.sh. Version bump.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd popup
+yarn install
+yarn build
+
+cd ..
+[[ -d web-ext-artifacts ]] || mkdir web-ext-artifacts
+FILENAME="$(basename "$PWD")-$(jq -r .version manifest.json).zip"
+zip -r "web-ext-artifacts/$FILENAME" LICENSE gmcr.js manifest.json popup/build/ icons/
+zip -r "web-ext-artifacts/source-$FILENAME" $(git ls-files)
