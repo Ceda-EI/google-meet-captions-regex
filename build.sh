6bcef99546e9245baaf2361d2b44bfc1b8c17f57
Ceda EI <ceda_ei@webionite.com>
2020-09-20 12:42:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ yarn build
 cd ..
 [[ -d web-ext-artifacts ]] || mkdir web-ext-artifacts
 FILENAME="$(basename "$PWD")-$(jq -r .version manifest.json).zip"
-zip -r "web-ext-artifacts/$FILENAME" LICENSE gmcr.js manifest.json popup/build/ icons/
+zip -r "web-ext-artifacts/$FILENAME" LICENSE gmcr.js manifest.json background.js popup/build/ icons/
 zip -r "web-ext-artifacts/source-$FILENAME" $(git ls-files)
